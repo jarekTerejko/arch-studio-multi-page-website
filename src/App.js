28431dc9d9f1457228082f5e71260da82332a1ae
jarekTerejko <j.terejko@gmail.com>
2022-01-23 04:34:12 +0100

    add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+
 function App() {
   return (
-    <div>
-      APP
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
