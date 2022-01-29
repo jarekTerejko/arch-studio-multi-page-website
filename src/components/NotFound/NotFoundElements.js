7eeeece2ef260a97a5d21855a34dbaf78e5d32a9
jarekTerejko <j.terejko@gmail.com>
2022-01-29 05:20:43 +0100

    create NotFound

---
+++ src/components/NotFound/NotFoundElements.js
@@ -0,0 +1,32 @@
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const NotFoundWrapper = styled(motion.div)`
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: var(--color-dark);
+  text-align: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+`;
+
+export const NotFoundHeading = styled.h1`
+  margin-bottom: 3rem;
+  font-size: 2rem;
+  max-width: 50rem;
+  text-align: center;
+  color: var(--color-white);
+`;
+
+export const NotFoundLink = styled(Link)`
+  color: var(--color-white);
+  font-size: 3rem;
+  margin: 3rem 0;
+  text-transform: uppercase;
+`;
