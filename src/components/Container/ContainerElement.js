f8ee1166415c4231b6baf7eafa23b9c78bb9e58d
jarekTerejko <j.terejko@gmail.com>
2022-01-29 05:30:48 +0100

    fix Container

--- src/components/Container/ContainerElement.js
+++ src/components/Container/ContainerElement.js
@@ -2,6 +2,14 @@ import styled from "styled-components";
 
 export const ContainerEl = styled.div`
   margin: 0 auto;
-  max-width: ${(props) => (props.big ? "144rem" : "115rem")};
-  padding: ${(props) => (props.big ? "0 16.5rem" : "0 2rem")};
+  max-width: 144rem;
+  padding: 0 16.5rem;
+
+  @media screen and (max-width: 1000px) {
+    padding: 0 10rem;
+  }
+  
+  @media screen and (max-width: 560px) {
+    padding: ${(props) => (props.fullWithMobile ? "0" : "0 2rem")};
+  }
 `;
