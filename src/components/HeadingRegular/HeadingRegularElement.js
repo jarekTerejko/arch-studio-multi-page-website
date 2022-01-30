52601acf7ead050637b9b7b3c9a5f9475ecb712a
jarekTerejko <j.terejko@gmail.com>
2022-01-30 06:55:51 +0100

    create HeadingRegularEl

---
+++ src/components/HeadingRegular/HeadingRegularElement.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HeadingRegularEl = styled.h2`
+  font-size: 5rem;
+  margin-bottom: 4rem;
+
+  @media screen and (max-width: 560px) {
+    font-size: 3rem;
+  }
+`;
