e5a34203f2a98138d7105e8bbf421fa103dc1785
jarekTerejko <j.terejko@gmail.com>
2022-02-05 06:25:31 +0100

    create Footer

---
+++ src/components/Footer/FooterElements.js
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+
+export const FooterWrapper = styled.footer`
+  margin: 0 auto;
+  max-width: 144rem;
+  padding: 0 16.5rem;
+
+  @media screen and (max-width: 1230px) {
+    padding: 0;
+  }
+`;
+
+export const FooterInnerWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  background: var(--color-grey);
+  position: relative;
+
+  &::after {
+    content: "";
+    position: absolute;
+    right: 0;
+    top: 0;
+    height: 100%;
+    width: 130px;
+    background: var(--color-white);
+
+    @media screen and (max-width: 768px) {
+      display: none;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    min-height: 38rem;
+  }
+`;
+
+export const FooterLogoWrapper = styled.div`
+  padding: 8rem 4rem;
+  background: var(--color-dark);
+
+  @media screen and (max-width: 900px) {
+    padding: 4.5rem 3rem;
+    max-width: 12rem;
+  }
+  @media screen and (max-width: 768px) {
+    transform: translateY(-60px);
+  }
+`;
+
+export const FooterNavItems = styled.ul`
+  list-style: none;
+  display: flex;
+  background: var(--color-grey);
+  margin-left: 6rem;
+
+  @media screen and (max-width: 900px) {
+    margin-left: 0;
+  }
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    margin-bottom: 2rem;
+    align-items: center;
+    transform: translateY(-20px);
+  }
+`;
+
+export const FooterMainBtnWrapper = styled.div`
+  position: absolute;
+  right: 0px;
+  z-index: 1;
+
+  @media screen and (max-width: 768px) {
+    position: static;
+  }
+`;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,55 @@
+import { ImageEl } from "../Image/ImageElement";
+import {
+  FooterInnerWrapper,
+  FooterLogoWrapper,
+  FooterMainBtnWrapper,
+  FooterNavItems,
+  FooterWrapper,
+} from "./FooterElements";
+import LogoWhite from "../../images/icons/logo-white.svg";
+import {
+  NavbarNavItem,
+  NavbarNavLinkR,
+  NavbarSiteLinkR,
+} from "../Navbar/NavbarElements";
+import MainBtn from "../MainBtn";
+
+const Footer = () => {
+  const paths = [
+    { name: "", path: "/" },
+    { name: "portfolio", path: "portfolio" },
+    { name: "about us", path: "about" },
+    { name: "contact", path: "contact" },
+  ];
+  return (
+    <>
+      <FooterWrapper>
+        <FooterInnerWrapper>
+          <FooterLogoWrapper>
+            <NavbarSiteLinkR to="/">
+              <ImageEl src={LogoWhite} alt="Logo" />
+            </NavbarSiteLinkR>
+          </FooterLogoWrapper>
+          <FooterNavItems>
+            {paths.map((link, i) => {
+              if (i === 0) {
+                return null;
+              } else {
+                return (
+                  <NavbarNavItem key={i}>
+                    <NavbarNavLinkR to={link.path}>{link.name}</NavbarNavLinkR>
+                  </NavbarNavItem>
+                );
+              }
+            })}
+          </FooterNavItems>
+          <FooterMainBtnWrapper>
+            <MainBtn pathname="about" text="see our portfolio" isText />
+          </FooterMainBtnWrapper>
+        </FooterInnerWrapper>
+      </FooterWrapper>
+    </>
+  );
+};
+
+export default Footer;
