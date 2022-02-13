548b95ae57f51bba425aa4cd05e93bb346a16943
jarekTerejko <j.terejko@gmail.com>
2022-02-13 06:20:29 +0100

    add IntroHeader

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,24 @@
+import { ContainerEl } from "../components/Container/ContainerElement";
+import IntroHeader from "../components/IntroHeader";
+import Loader from "../components/Loader";
+import useFetch from "../useFetch";
+
 const About = () => {
-    return (
-      <>
-        <h1>About page</h1>
-      </>
-    );
-}
- 
+  const { data, isLoading, fetchErrorValue } = useFetch("../data.json");
+  return (
+    <>
+      {isLoading ? <Loader /> : null}
+      {data.length === 0 ? (
+        <ContainerEl>
+          <h1>{fetchErrorValue}</h1>
+        </ContainerEl>
+      ) : (
+        <>
+          <IntroHeader data={data[0].aboutData} />
+        </>
+      )}
+    </>
+  );
+};
+
 export default About;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,22 @@
+import { ContainerEl } from "../components/Container/ContainerElement";
+import Loader from "../components/Loader";
+import IntroHeader from "../components/IntroHeader";
+import useFetch from "../useFetch";
+
 const Contact = () => {
+  const { data, isLoading, fetchErrorValue } = useFetch("../data.json");
   return (
     <>
-      <h1>Contact page</h1>
+      {isLoading ? <Loader /> : null}
+      {data.length === 0 ? (
+        <ContainerEl>
+          <h1>{fetchErrorValue}</h1>
+        </ContainerEl>
+      ) : (
+        <>
+          <IntroHeader data={data[0].contactData} />
+        </>
+      )}
     </>
   );
 };
