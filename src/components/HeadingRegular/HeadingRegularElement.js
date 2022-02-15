9e08fb5600ba936a02a2718c0d8b7bc934c40388
jarekTerejko <j.terejko@gmail.com>
2022-02-15 05:47:40 +0100

    add alternate color by props

--- src/components/HeadingRegular/HeadingRegularElement.js
+++ src/components/HeadingRegular/HeadingRegularElement.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const HeadingRegularEl = styled.h2`
   font-size: 5rem;
   margin-bottom: 4rem;
+  color: ${({ textWhite }) => (textWhite ? "var(--color-white)" : "")};
 
   @media screen and (max-width: 560px) {
     font-size: 3rem;
