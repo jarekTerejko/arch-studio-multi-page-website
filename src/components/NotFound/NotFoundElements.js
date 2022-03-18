e5f26e8aa5cacf0e69a53bcfdc5bdfe96122d4e9
jarekTerejko <j.terejko@gmail.com>
2022-03-18 05:21:13 +0100

    fix z-index

--- src/components/NotFound/NotFoundElements.js
+++ src/components/NotFound/NotFoundElements.js
@@ -14,6 +14,7 @@ export const NotFoundWrapper = styled(motion.div)`
   position: fixed;
   top: 0;
   left: 0;
+  z-index: 2;
 `;
 
 export const NotFoundHeading = styled.h1`
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,29 +1,14 @@
 import { AnimatePresence } from "framer-motion";
-import { NotFoundHeading, NotFoundLink, NotFoundWrapper } from "./NotFoundElements";
+import {
+  NotFoundHeading,
+  NotFoundLink,
+  NotFoundWrapper,
+} from "./NotFoundElements";
 
 const NotFound = () => {
-  const containerVariants = {
-    hidden: {
-      opacity: 0,
-    },
-    visible: {
-      opacity: 1,
-      transition: { delay: 0.1, duration: 1 },
-    },
-    exit: {
-      opacity: 0,
-      transition: { ease: "easeInOut" },
-    },
-  };
-
   return (
     <AnimatePresence exitBeforeEnter>
-      <NotFoundWrapper
-        variants={containerVariants}
-        initial="hidden"
-        animate="visible"
-        exit="exit"
-      >
+      <NotFoundWrapper>
         <NotFoundHeading>404 This page could not be found</NotFoundHeading>
         <NotFoundLink to="/">Home</NotFoundLink>
       </NotFoundWrapper>
